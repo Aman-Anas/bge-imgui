9638a471807f910860dc47b48500414632cdbf9e
Aman Anas <IA.AmanAnas@gmail.com>
2023-07-17 12:57:46 -0400

    Add imgui demo windows

--- bgimgui/imguiWrapper.py
+++ bgimgui/imguiWrapper.py
@@ -12,7 +12,9 @@ class BGEImguiWrapper:
         bge.logic.gui = self
         self.imgui_context = imgui.create_context()
         self.imgui_backend = BGEImguiRenderer(scene)
+
         self.show_custom_window = True
+
         self.guiThread = Thread(target=self.runGUIThread)
         self.guiThread.start()
         bge.logic.mouse.visible = True
@@ -24,11 +26,12 @@ class BGEImguiWrapper:
 
     def run(self):
         backend = self.imgui_backend
+
         backend.updateIO()
 
         imgui.new_frame()
 
-        if True:
+        if False:
             is_expand, self.show_custom_window = imgui.begin(
                 "Custom window", True)
             if is_expand:
@@ -39,7 +42,7 @@ class BGEImguiWrapper:
                 imgui.extra.text_ansi_colored("Eggs", 0.2, 1.0, 0.0)
 
             imgui.end()
-        if True:
+        if False:
             # open new window context
             imgui.begin("Your first window!", True)
 
@@ -47,6 +50,29 @@ class BGEImguiWrapper:
             imgui.text("Hello world!")
 
             imgui.end()
-        # backend.process_inputs()
+
+        if imgui.begin_main_menu_bar():
+            if imgui.begin_menu("File", True):
+
+                clicked_quit, selected_quit = imgui.menu_item(
+                    "Quit", "Cmd+Q", False, True
+                )
+
+                if clicked_quit:
+                    sys.exit(0)
+
+                imgui.end_menu()
+            imgui.end_main_menu_bar()
+
+        imgui.show_test_window()
+
+        if self.show_custom_window:
+            is_expand, self.show_custom_window = imgui.begin(
+                "Custom window", True)
+            if is_expand:
+                imgui.text("Bar")
+                imgui.text_colored("Eggs", 0.2, 1.0, 0.0)
+            imgui.end()
+
         imgui.render()
         backend.render(imgui.get_draw_data())
