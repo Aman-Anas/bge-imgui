9c3f68961ffb0d4566c5392ce03e1d5a808b3d4b
mbanas <IA.AmanAnas@gmail.com>
2023-07-17 18:30:35 -0400

    Remove extraneous comments

--- bgimgui/main.py
+++ bgimgui/main.py
@@ -14,10 +14,8 @@ def runGUI(cont: SCA_PythonController):
         own = cont.owner
         gui: BGEImguiWrapper = bge.logic.gui
         gui.updateOnGameFrame()
-        pass
 
-        # right now all gui functions are in the class
-        
+
 def stopGUI(cont: SCA_PythonController):
     if cont.sensors["stop"].positive:
         own = cont.owner
