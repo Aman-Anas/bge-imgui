e0c39eff56f8308871435157aa3f6b32c4ed66d8
mbanas <IA.AmanAnas@gmail.com>
2023-07-17 20:41:54 -0400

    Extract custom UI functionality to child class

--- bgimgui/main.py
+++ bgimgui/main.py
@@ -1,4 +1,4 @@
-from .imguiWrapper import BGEImguiWrapper
+from .myGUI import MyCustomGUI
 from bge.types import SCA_PythonController
 import bge
 
@@ -6,19 +6,19 @@ import bge
 def startGUI(cont: SCA_PythonController):
     if cont.sensors["tap"].positive:
         own = cont.owner
-        gui = BGEImguiWrapper(own.scene)
+        gui = MyCustomGUI(own.scene)  # Initialize the gui
 
 
 def runGUI(cont: SCA_PythonController):
     if cont.sensors["loop"].positive:
         own = cont.owner
-        gui: BGEImguiWrapper = bge.logic.gui
+        gui: MyCustomGUI = bge.logic.gui
         gui.updateOnGameFrame()
 
 
 def stopGUI(cont: SCA_PythonController):
     if cont.sensors["stop"].positive:
         own = cont.owner
-        gui: BGEImguiWrapper = bge.logic.gui
+        gui: MyCustomGUI = bge.logic.gui
         gui.shutdownGUI()
         bge.logic.endGame()
