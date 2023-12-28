f7c6f555b02c9bba2e7662cd4e73a3df9ba03178
Aman Anas <IA.AmanAnas@gmail.com>
2023-12-28 13:24:23 -0500

    Simple example script

---
+++ my_game/simple.py
@@ -0,0 +1,25 @@
+from .simple_gui import SimpleCustomGUI
+from bge.types import SCA_PythonController
+import bge
+
+
+def startGUI(cont: SCA_PythonController):
+    if cont.sensors["tap"].positive:
+        own = cont.owner
+        # Initialize the gui
+        gui = SimpleCustomGUI(own.scene)
+
+
+def runGUI(cont: SCA_PythonController):
+    if cont.sensors["loop"].positive:
+        own = cont.owner
+        gui: SimpleCustomGUI = bge.logic.gui
+        gui.update_gui()
+
+
+def stopGUI(cont: SCA_PythonController):
+    if cont.sensors["stop"].positive:
+        own = cont.owner
+        gui: SimpleCustomGUI = bge.logic.gui
+        gui.shutdown_gui()
+        bge.logic.endGame()
