589d86257254cb56d73954ff2eda24a4b72a56c5
Aman Anas <IA.AmanAnas@gmail.com>
2024-01-01 09:42:26 -0500

    Implement wrapper for panel rendering system

--- my_game/panel_3d.py
+++ my_game/panel_3d.py
@@ -1,4 +1,5 @@
-from .my_gui import MyCustomGUI
+from . import bgimgui
+from .simple_gui import SimpleCustomGUI
 from bge.types import SCA_PythonController
 import bge
 
@@ -7,20 +8,23 @@ def startGUI(cont: SCA_PythonController):
     if cont.sensors["tap"].positive:
         own = cont.owner
         # Initialize the gui
-        bge.logic.gui = MyCustomGUI(
-            own.scene, bge.logic.expandPath("//cursors"))
+        own["gui"] = SimpleCustomGUI(
+            own.scene, main=False, panel=own, resolution=(400, 400))
+
+        # This is how you would change the global panel interaction distance
+        # bgimgui.BGEImguiRenderer.INTERACTION_DIST = 3
 
 
 def runGUI(cont: SCA_PythonController):
     if cont.sensors["loop"].positive:
         own = cont.owner
-        gui: MyCustomGUI = bge.logic.gui
+        gui: SimpleCustomGUI = own["gui"]
         gui.update_gui()
 
 
 def stopGUI(cont: SCA_PythonController):
     if cont.sensors["stop"].positive:
         own = cont.owner
-        gui: MyCustomGUI = bge.logic.gui
+        gui: SimpleCustomGUI = own["gui"]
         gui.shutdown_gui()
         bge.logic.endGame()
