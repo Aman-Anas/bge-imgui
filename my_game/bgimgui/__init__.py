b9784e0c3e0ad04bb197027cbdbb7dfdc4b4a96b
Aman Anas <IA.AmanAnas@gmail.com>
2024-01-25 12:44:06 -0500

    Fix import script

--- my_game/bgimgui/__init__.py
+++ my_game/bgimgui/__init__.py
@@ -2,7 +2,7 @@ import os
 import sys
 import pathlib
 import numpy as np
-
+from . import import_fix
 # What follows is an incredibly cursed workaround for a bug when importing imgui_bundle
 # via the embedded player multiple times.
 
@@ -17,21 +17,10 @@ if not imgui_path.exists():
 with open(imgui_path, 'r') as init_file:
     src = init_file.readlines()
 
-if src[0] != "#BGE_PATCHED\n":
+if src[0] != "#BGE_PATCHED_2\n":
     print("Patching imgui_bundle for BGE...")
-    src.insert(0, "try:\n")
-    src.insert(0, "#BGE_PATCHED\n")
-    for index, line in enumerate(src):
-        if line.startswith("from"):
-            src[index] = f"    {line}"
-        if "glfw" in line:
-            insert_point = index
-            break
-    src.insert(insert_point,
-               "except (ImportError, ModuleNotFoundError):\n    pass\n")
-
     with open(imgui_path, 'w+') as init_file:
-        init_file.write("".join(src))
+        init_file.write(import_fix.data)
     print("Patch complete!")
 
 # Now that the patch is complete, resume normal imports
